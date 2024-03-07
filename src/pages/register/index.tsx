41647f6212b1dbda97b0cd4aa09f49ab64c4cc14
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-03-07 12:39:53 +0000

    Correct emoji count

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -44,7 +44,8 @@ export default function Register({ apiUrl }: BaseProps) {
     }
 
     // Check emojis
-    const emojiRegex = /\p{Emoji}/u;
+    const emojiRegex =
+      /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gi;
     const emojiCount = (input.match(emojiRegex) || []).length;
     if (emojiCount < config.emoji) {
       return `Password must contain at least ${config.emoji} emojis.`;
